18a2e004470a19c25c37f646a5cfd02b4a8e5ea2
Wilson <wilson.santiago.car@gmail.com>
2025-01-22 16:58:50 -0500

    headless ui installed, search bar component created

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+"use client";
 
-const SearchManufacturer = () => {
-  return <div>SearchManufacturer</div>;
+import { SearchManufacturerProps } from "@/types";
+import React from "react";
+import { Combobox, Transition } from "@headlessui/react";
+
+const SearchManufacturer = ({
+  manufacturer,
+  setManufacturer,
+}: SearchManufacturerProps) => {
+  return (
+    <div className="seach-manufacturer">
+      <Combobox>
+        <div className="relative w-full"></div>
+      </Combobox>
+    </div>
+  );
 };
 
 export default SearchManufacturer;
